4173005b9c2e00f4165a36bd1fcc302ed1373951
Kirill Müller <krlmlr+github@mailbox.org>
2024-03-30 16:45:04 +0000

    Simplify connectivity and testing

--- compose_files/test/test-postgres.R
+++ compose_files/test/test-postgres.R
@@ -1,18 +1,6 @@
 
 # simple test script
 
-print('Simple Test of Postgres Connectivity')
-
-con <- try(DBI::dbConnect(
-  RPostgres::Postgres(),
-  host = 'postgres',
-  user = 'compose',
-  password = 'YourStrong!Passw0rd'), silent = F)
-
-if(class(con) == "try-error"){
-  print('Incorrect Setup.')
-}else{
-  print(con)
-  print('Connection Correctly Configured.')
-}
-
+message("Simple Test of Postgres Connectivity")
+RPostgres::postgresDefault()
+message("Connection established successfully")
