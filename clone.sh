2b8d64c5f300c7f66e9e1bd14b86093f89e48e91
Kirill Müller <kirill@cynkra.com>
2024-03-30 16:22:51 +0100

    feat: Cloning script

---
+++ clone.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p r-dbi
+cd r-dbi
+
+gh repo clone r-dbi/DBI
+gh repo clone r-dbi/RMariaDB
+gh repo clone r-dbi/RSQLite
+gh repo clone r-dbi/RPostgres
+gh repo clone r-dbi/odbc
+gh repo clone r-dbi/adbi
